80e3bb834aeb5e13af301eeb4337ff83c1a4ffaa
Bethlehem-Hailegebrel <kiyakeys7@gmail.com>
2023-11-13 03:00:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A C program that prints a line with puts fuction
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+       	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
